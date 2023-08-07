bdb9c4877ea9cd1c86a8218d4fa6eb0d22b2f621
Bohdan <yashbon@ukr.net>
2023-08-08 01:34:21 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,28 +4,33 @@ import { RegistrationScreen } from "./src/Screens/RegistrationScreen";
 import { LoginScreen } from "./src/Screens/LoginScreen";
 import { PostsScreen } from "./src/Screens/PostsScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+
 import Home from "./src/Screens/Home";
 
 const App = () => {
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-        },
-        text: {
-            fontSize: 42,
-            lineHeight: 84,
-            fontWeight: "bold",
-            textAlign: "center",
-        },
-    });
     return (
-        <View style={styles.container}>
-            {/* <Home /> */}
-            {/* <RegistrationScreen /> */}
-            <LoginScreen />
-            {/* <PostsScreen /> */}
-        </View>
+        <NavigationContainer>
+            <View style={styles.container}>
+                {/* <Home /> */}
+                {/* <RegistrationScreen /> */}
+                <LoginScreen />
+                {/* <PostsScreen /> */}
+            </View>
+        </NavigationContainer>
     );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    text: {
+        fontSize: 42,
+        lineHeight: 84,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
